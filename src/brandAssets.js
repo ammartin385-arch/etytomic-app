751017a73369977c5fdf895887150588822f386e
ammartin385-arch <am.martin385@gmail.com>
2026-06-06 09:47:47 -0400

    rebrand fix

--- src/brandAssets.js
+++ src/brandAssets.js
@@ -1,9 +1,10 @@
 export const BRAND_ASSETS = Object.freeze({
-  mark: "/brand/logo-mark.svg",
+  mark: "/brand/logo-mark.png",
   horizontal: "/brand/logo-horizontal.png",
   stacked: "/brand/logo-stacked.png",
   emailLogo: "/brand/logo-horizontal.png",
   appIcon: "/brand/app-icon.png",
+  favicon: "/brand/logo-mark.png",
 });
 
 if (typeof window !== "undefined") {
